2eb27efabd7aa1df210bd47de67bf73c1fb08abb
Govinda Mandal <govinda4india@gmail.com>
2021-04-22 17:47:56 +0530

    Second Commit

---
+++ src/Service/NexiPaymentService.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NexiPayment\Service;
+
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStateHandler;
+use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
+use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\AsynchronousPaymentHandlerInterface;
+use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentProcessException;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Shopware\Core\Checkout\Payment\Exception\CustomerCanceledAsyncPaymentException;
+use Symfony\Component\HttpFoundation\Request;
+
+class NexiPaymentService implements AsynchronousPaymentHandlerInterface
+{
+    /**
+     * @var $OrderTransactionStateHandler
+     */
+    private $transactionStateHandler;
+
+    public function __construct(OrderTransactionStateHandler $transactionStateHandler)
+    {
+        $this->transactionStateHandler = $transactionStateHandler;
+    }
+
+    public function pay(AsyncPaymentTransactionStruct $transaction, RequestDataBag $dataBag, SalesChannelContext $salesChannelContext): RedirectResponse
+    {
+        // Method that sends the return URL to the external gateway and gets a redirect URL back
+        try {
+            $redirectUrl = $this->sendReturnUrlToExternalGateway($transaction->getReturnUrl());
+        } catch (\Exception $e) {
+            throw new AsyncPaymentProcessException(
+                $transaction->getOrderTransaction()->getId(),
+                'An error occurred during the communication with external payment gateway' . PHP_EOL . $e->getMessage()
+            );
+        }
+
+        // Redirect to external gateway
+        return new RedirectResponse($redirectUrl);
+    }
+
+    /**
+     * @throws CustomerCanceledAsyncPaymentException
+     */
+
+    public function finalize(AsyncPaymentTransactionStruct $transaction, Request $request, SalesChannelContext $salesChannelContext): void
+    {
+        $transactionId = $transaction->getOrderTransaction()->getId();
+
+        // Cancelled payment?
+        if ($request->query->getBoolean('cancel')) {
+            throw new CustomerCanceledAsyncPaymentException(
+                $transactionId,
+                'Customer canceled the payment on the PayPal page'
+            );
+        }
+
+        $paymentState = $request->query->getAlpha('status');
+
+        $context = $salesChannelContext->getContext();
+        if ($paymentState === 'completed') {
+            // Payment completed, set transaction status to "paid"
+            $this->transactionStateHandler->paid($transaction->getOrderTransaction()->getId(), $context);
+        } else {
+            // Payment not completed, set transaction status to "open"
+            $this->transactionStateHandler->reopen($transaction->getOrderTransaction()->getId(), $context);
+        }
+    }
+
+    private function sendReturnUrlToExternalGateway(string $returnUrl): string
+    {
+        // Must be provided by the PaymentProvider after you send the request data
+        $paymentProviderUrl = null;
+
+        $requestData = [
+            // more data like amount, items, etc
+            'returnUrl' => $returnUrl,
+        ];
+
+        // Do API Call to your payment provider with the request data and get back the $paymentProviderUrl
+        return $paymentProviderUrl;
+    }
+}
